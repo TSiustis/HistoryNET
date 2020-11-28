acfafde6d93cfd477ef975c88ff1c47bff894ac1
Siustis Teodor <siustis.teodor@gmail.com>
2020-11-28 18:34:05 +0200

    Added project

---
+++ History/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace History.Shared
+{
+   public class Event
+    {
+        public int Year { get; set; }
+        public string Url { get; set; }
+        public string Content { get; set; }
+        public Link Link { get; set; }
+    }
+}
